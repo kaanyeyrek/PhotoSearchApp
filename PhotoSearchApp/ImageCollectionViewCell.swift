127855cca4f8db031f712681ccc96c9a51c80ba9
Kaan Yeyrek <kaanyeyrek@icloud.com>
2022-08-08 16:57:09 +0300

    first

---
+++ PhotoSearchApp/ImageCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImageCollectionViewCell.swift
+//  PhotoSearchApp
+//
+//  Created by Kaan Yeyrek on 8/8/22.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
